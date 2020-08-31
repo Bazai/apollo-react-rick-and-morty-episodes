36ce9304270bd29e6ba832bd2a0b4771423a3e2b
Pavel Bubentsov <bazzy.bazzy@gmail.com>
2020-08-31 16:56:49 +0300

    Кривая загрузка всех эпизодов

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -1,31 +1,112 @@
-import React from "react";
-// import { graphql } from "@apollo/react-hoc";
-// import { loader } from "graphql.macro";
-// const episodesQuery = loader("../queries/episodes.graphql");
-import { useApolloClient } from "@apollo/client";
-import { gql } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const EPISODES = gql`
+  query GetEpisodes {
+    episodes(page: 1) {
+      info {
+        count
+      }
+      results {
+        id
+        name
+        air_date
+        created
+        episode
+      }
+    }
+  }
+`;
+const EPISODES2 = gql`
+  query GetEpisodes {
+    episodes(page: 2) {
+      info {
+        count
+      }
+      results {
+        id
+        name
+        air_date
+        created
+        episode
+      }
+    }
+  }
+`;
+const EPISODES3 = gql`
+  query GetEpisodes {
+    episodes(page: 3) {
+      info {
+        count
+      }
+      results {
+        id
+        name
+        air_date
+        created
+        episode
+      }
+    }
+  }
+`;
 
 const Episodes = () => {
-  const client = useApolloClient();
-  client
-    .query({
-      query: gql`
-        query GetEpisodes {
-          episodes(page: 1) {
-            info {
-              count
-            }
-            results {
-              id
-              name
-            }
-          }
-        }
-      `,
-    })
-    .then((result) => console.log(result));
-  return <h1 className="mt-4 font-bold text-3xl text-black">Episodes</h1>;
+  // const client = useApolloClient();
+  const [episodes, setEpisodes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { loading, error, data } = useQuery(EPISODES);
+  const { loading: loading2, error: error2, data: data2 } = useQuery(
+    EPISODES2,
+    {
+      skip: !data,
+    }
+  );
+
+  const { loading: loading3, error: error3, data: data3 } = useQuery(
+    EPISODES3,
+    {
+      skip: !data2,
+    }
+  );
+
+  useEffect(() => {
+    setEpisodes(data?.episodes?.results);
+  }, [data]);
+
+  useEffect(() => {
+    if (data2?.episodes?.results) {
+      setEpisodes((state) => {
+        return [...state, ...data2?.episodes?.results];
+      });
+    }
+  }, [data2]);
+
+  useEffect(() => {
+    if (data3?.episodes?.results) {
+      setEpisodes((state) => {
+        return [...state, ...data3?.episodes?.results];
+      });
+      setIsLoading(false);
+    }
+  }, [data3]);
+
+  useEffect(() => {
+    console.log("EEE", episodes);
+  }, [episodes]);
+
+  useEffect(() => {
+    console.log("II", isLoading);
+  }, [isLoading]);
+
+  // if (error) {
+  //   return <h1 className="mt-4 font-bold text-3xl text-red-900">Error</h1>;
+  // } else
+  if (isLoading) {
+    return <h1 className="mt-4 font-bold text-3xl text-black">Loading...</h1>;
+  } else {
+    return <h1 className="mt-4 font-bold text-3xl text-black">Episodes</h1>;
+  }
 };
 
-// export default graphql(episodesQuery)(Episodes);
 export default Episodes;
